dd07628d237f7734b307dac4478491daa7d1014c
Marko Kungla <marko@techtribe.nl>
2016-10-07 01:08:03 +0200

    Add build tasks

--- scripts/travis/after_success.sh
+++ scripts/travis/after_success.sh
@@ -12,7 +12,7 @@ if find "${TRAVIS_BUILD_DIR}/vendor/server" -mindepth 1 -print -quit | grep -q .
 elif find "${TRAVIS_BUILD_DIR}/vendor/node_modules" -mindepth 1 -print -quit | grep -q .; then
     log_info "Running post success tasks for Toolshedr UI  ..."
 else
-    log_err "Something is wrong nor TRAVIS_PHP_VERSION or TRAVIS_NODE_VERSION are set!"
+    log_err "Something is wrong - Can't find nor NPM or Composer dependencies!"
     exit 1
 fi
 exit 0
