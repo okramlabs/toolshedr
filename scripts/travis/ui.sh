bc37a43daf7399721f00bcd9e498b36e05ed5436
Marko Kungla <marko@techtribe.nl>
2016-10-06 22:53:47 +0200

    Rename buildscripts to travis
    to enable use of other build systems

--- scripts/travis/ui.sh
+++ scripts/travis/ui.sh
@@ -4,6 +4,7 @@ set -ev
 echo "Loaded Toolshedr UI build script!"
 
 if [ "${TRAVIS_NODE_VERSION}" != "false" ]; then
+    export NPM_CONFIG_PREFIX=${TRAVIS_BUILD_DIR}/vendor/node_modules
 	echo ${TRAVIS_NODE_VERSION}
 else
     echo "UI called"
