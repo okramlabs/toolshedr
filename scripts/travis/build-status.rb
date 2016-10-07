e09c0bcae6ae20c2b29b0606fcfc7d4222c5cd1f
Marko Kungla <marko@techtribe.nl>
2016-10-08 01:55:32 +0200

    Add Travis ...

--- scripts/travis/build-status.rb
+++ scripts/travis/build-status.rb
@@ -5,16 +5,20 @@ require 'waitutil'
 
 # if puts rails.job('21.1').red? exit
 
-
-
-def are_builds_finished(build_nr)
-  puts "Checking are previous jobs done ..."
+##
+# Chech are following build completed!
+# 1. test-php-min
+# 2. test-php-stable
+# 3. test-npm-min
+# 4. test-npm-stable
+##
+def are_builds_finished(build_nr, attempt)
+  printf("Checking are previous jobs done ... Waiting already for %d seconds", attempt)
   toolshedr = Travis::Repository.find('mkungla/toolshedr')
   return (toolshedr.job(sprintf("%d.1", build_nr)).finished? &&
       toolshedr.job(sprintf("%d.2", build_nr)).finished? &&
       toolshedr.job(sprintf("%d.3", build_nr)).finished? &&
-      toolshedr.job(sprintf("%d.4", build_nr)).finished? &&
-      toolshedr.job(sprintf("%d.5", build_nr)).finished?
+      toolshedr.job(sprintf("%d.4", build_nr)).finished?
   )
 end
 
@@ -23,8 +27,7 @@ def are_we_ready_to_continue(build_nr)
   return (toolshedr.job(sprintf("%d.1", build_nr)).green? &&
       toolshedr.job(sprintf("%d.2", build_nr)).green? &&
       toolshedr.job(sprintf("%d.3", build_nr)).green? &&
-      toolshedr.job(sprintf("%d.4", build_nr)).green? &&
-      toolshedr.job(sprintf("%d.5", build_nr)).green?
+      toolshedr.job(sprintf("%d.4", build_nr)).green?
   )
 
 end
@@ -34,7 +37,7 @@ attempt = 1
 WaitUtil.wait_for_condition('other jobs to finish', :verbose => true, :timeout_sec => 600, :delay_sec => 15) do
   attempt += 1
 
-  [are_builds_finished(ENV['TRAVIS_BUILD_NUMBER'] ), "Seems that jobs take way to long to finish!"]
+  [are_builds_finished(ENV['TRAVIS_BUILD_NUMBER'], attempt), "Seems that jobs take way to long to finish!"]
 end
 
-exit are_we_ready_to_continue(ENV['TRAVIS_BUILD_NUMBER'] ) ? 0 : 1
+exit are_we_ready_to_continue(ENV['TRAVIS_BUILD_NUMBER']) ? 0 : 1
