e85228a80a80c73c0287e062b5289a20306f6ad5
Marko Kungla <marko@techtribe.nl>
2016-10-08 03:03:58 +0200

    Travis configuration

--- scripts/travis/before-script.sh
+++ scripts/travis/before-script.sh
@@ -4,7 +4,7 @@ set -e
 TCI_RUNNER_PWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Import dependencies
-source "$TRAVIS_BUILD_DIR/../helpers/colors.sh"
+source "$TCI_RUNNER_PWD/../helpers/colors.sh"
 
 if [ "${TRAVIS_JOB_NUMBER:${#TRAVIS_JOB_NUMBER}-1:1}" -eq 5 ]; then
     log_info "Waiting other jobs to finish before we decide either to release or not!!!"  
