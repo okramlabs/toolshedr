f314728ffe5c786083d8c1be5fc1f8deb70f99a7
Marko Kungla <marko@techtribe.nl>
2016-10-08 03:01:53 +0200

    Travis configuration

--- scripts/travis/before-script.sh
+++ scripts/travis/before-script.sh
@@ -4,11 +4,10 @@ set -e
 TCI_RUNNER_PWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Import dependencies
-source "$TCI_RUNNER_PWD/../helpers/colors.sh"
+source "$TRAVIS_BUILD_DIR/../helpers/colors.sh"
 
-if [ "${TRAVIS_JOB_NUMBER:${#TRAVIS_JOB_NUMBER}-1:1}" == 5 ]; then
+if [ "${TRAVIS_JOB_NUMBER:${#TRAVIS_JOB_NUMBER}-1:1}" -eq 5 ]; then
     log_info "Waiting other jobs to finish before we decide either to release or not!!!"  
-elif [ "${TRAVIS_JOB_NUMBER:${#TRAVIS_JOB_NUMBER}-1:1}" < 5 ]; then
+elif [ "${TRAVIS_JOB_NUMBER:${#TRAVIS_JOB_NUMBER}-1:1}" -lt 5 ]; then
     log_info "Shared before_script..."
 fi
-
