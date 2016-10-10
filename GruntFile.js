b6dc2efd6a170837eab411c2e5129ada10276847
Marko Kungla <marko@techtribe.nl>
2016-10-10 02:00:45 +0200

    Strings must use doublequote. (quotes)

--- GruntFile.js
+++ GruntFile.js
@@ -1,11 +1,11 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     
     browserify: {
       devel: {
         options: {
-          transform: [['babelify', {
+          transform: [["babelify", {
             presets: ["es2015"],
             plugins: [
               ["babel-plugin-transform-builtin-extend", {
@@ -15,66 +15,66 @@ module.exports = function (grunt) {
           }]]
         },
         files: {
-          './devel/js/toolshedr.js': ['./src/app/js/index.js']
+          "./devel/js/toolshedr.js": ["./src/app/js/index.js"]
         }
       },
       dist: {
         options: {
-          transform: [['babelify', {presets: ["es2015"]}]]
+          transform: [["babelify", {presets: ["es2015"]}]]
         },
-        src: './src/app/js/index.js',
-        dest: './dist/js/toolshedr.js'
+        src: "./src/app/js/index.js",
+        dest: "./dist/js/toolshedr.js"
       }
     },
     jshint: {
       options: {
         force: true,
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
+        jshintrc: ".jshintrc",
+        reporter: require("jshint-stylish")
       },
       devel: {
-        src: ['./src/app/js/**/*.js'],
+        src: ["./src/app/js/**/*.js"],
       },
     },
     compass: {
       devel: {
         options: {
-          sassDir: './src/app/scss',
-          cssDir: './devel/css',
-          cacheDir: './tmp/.sass-cache'
+          sassDir: "./src/app/scss",
+          cssDir: "./devel/css",
+          cacheDir: "./tmp/.sass-cache"
         }
       },
       dist: {
         options: {
-          cssDir: './dist/css',
-          sassDir: './src/app/scss',
+          cssDir: "./dist/css",
+          sassDir: "./src/app/scss",
           sourcemap: true,
-          outputStyle: 'compressed',
-          environment: 'production'
+          outputStyle: "compressed",
+          environment: "production"
         }
       }
     },
     uglify: {
       options: {
-        banner: '/*! \n' +
-        ' * Package: <%= pkg.name %> \n' +
-        ' * Version: <%= pkg.version %>\n' +
-        ' * Released: <%= grunt.template.today("dd mmm yyyy") %> \n' +
-        ' * License: MIT \n' +
-        ' * Homepage: <%= pkg.homepage %> \n' +
-        '*/\n'
+        banner: "/*! \n" +
+        " * Package: <%= pkg.name %> \n" +
+        " * Version: <%= pkg.version %>\n" +
+        " * Released: <%= grunt.template.today('dd mmm yyyy') %> \n" +
+        " * License: MIT \n" +
+        " * Homepage: <%= pkg.homepage %> \n" +
+        "*/\n"
       },
       dist: {
         files: {
-          'dist/js/toolshedr.min.js': 'dist/js/toolshedr.js'
+          "dist/js/toolshedr.min.js": "dist/js/toolshedr.js"
         }
       }
     }
     ,
     watch: {
       css: {
-        files: ['./src/app/scss/**/*.scss'],
-        tasks: ['compass:devel'],
+        files: ["./src/app/scss/**/*.scss"],
+        tasks: ["compass:devel"],
         options: {
           livereload: true,
         }
@@ -82,8 +82,8 @@ module.exports = function (grunt) {
       }
       ,
       scripts: {
-        files: ['./src/app/js/**/*.js'],
-        tasks: ['jshint:devel', 'browserify:devel'],
+        files: ["./src/app/js/**/*.js"],
+        tasks: ["jshint:devel", "browserify:devel"],
         options: {
           livereload: true,
         }
@@ -94,29 +94,28 @@ module.exports = function (grunt) {
     connect: {
       server: {
         options: {
-          hostname: 'localhost',
+          hostname: "localhost",
           port: 9000,
-          base: 'devel',
+          base: "devel",
           livereload: true
         }
       }
     }
-  })
-  ;
-
-  grunt.event.on('watch', function (action, filepath) {
-    grunt.config('jshint.all.src', filepath);
   });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.event.on("watch", function (action, filepath) {
+    grunt.config("jshint.all.src", filepath);
+  });
 
-  grunt.registerTask('default', ['devel']);
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-compass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
 
-  grunt.registerTask('devel', ['connect', 'watch']);
-  grunt.registerTask('release', ["browserify:dist", "compass:dist", "uglify:dist"]);
+  grunt.registerTask("default", ["devel"]);
+
+  grunt.registerTask("devel", ["connect", "watch"]);
+  grunt.registerTask("release", ["browserify:dist", "compass:dist", "uglify:dist"]);
 };
